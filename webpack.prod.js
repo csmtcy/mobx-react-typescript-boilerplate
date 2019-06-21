60585b8a5457bc47702da1d42be03c9d9e5acf9d
Mark Tan <mtcy.9012@gmail.com>
2019-06-22 01:23:59 +0800

    remove entry

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,6 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-  entry: './src/index',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'build'),
